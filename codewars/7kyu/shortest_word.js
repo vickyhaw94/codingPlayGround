b3cf9cb3a0bbd87e855ecaa69ac0bd4bfde1ce0d
vickyhaw94 <weikee070194@gmail.com>
2016-12-06 21:09:22 +0800

    x Simple, given a string of words, return the length of the shortest word(s).
    String will never be empty and you do not need to account for different data types.

---
+++ codewars/7kyu/shortest_word.js
@@ -0,0 +1,23 @@
+/*
+x Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+
+*/
+
+function findShort(s){
+  var arr = [];
+  arr = s.split(" ");
+  var min = arr[0].length;
+  for(var i = 0; i <arr.length ; i++){
+    if(arr[i].length < min){
+      min = arr[i].length;
+    }
+  }
+  return min;
+}
+
+/*One of the best solution*/
+function findShort(s){
+    return Math.min(...s.split(" ").map (s => s.length));
+}
